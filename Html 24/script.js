d19500c003f3c164234ad7f124298eced6dd0aea
Vishesh-dobhal <148621361+Vishesh-dobhal@users.noreply.github.com>
2025-12-12 20:23:52 +0530

    Add files via upload
    add basic.html,subjects.html,photo.html,address.htmlsome changes onm dashboard

--- Html 24/script.js
+++ Html 24/script.js
@@ -29,3 +29,45 @@ const mobileEditMenu = document.getElementById("mobileEditMenu");
 mobileEditBtn.addEventListener("click", () => {
   mobileEditMenu.classList.toggle("hidden");
 });
+
+
+// SUBJECT MODAL
+const openSubjectModal = document.getElementById("openSubjectModal");
+const subjectModalOverlay = document.getElementById("subjectModalOverlay");
+const modalBox = document.getElementById("modalBox");
+const closeModal = document.getElementById("closeModal");
+const cancelModal = document.getElementById("cancelModal");
+
+// Open Modal
+openSubjectModal.addEventListener("click", () => {
+  subjectModalOverlay.classList.remove("hidden");
+  subjectModalOverlay.classList.add("flex");
+
+  setTimeout(() => {
+    modalBox.classList.remove("-translate-y-10", "opacity-0");
+    modalBox.classList.add("translate-y-0", "opacity-100");
+  }, 10);
+});
+
+// Close Modal Function
+function hideModal() {
+  modalBox.classList.add("-translate-y-10", "opacity-0");
+  modalBox.classList.remove("translate-y-0", "opacity-100");
+
+  setTimeout(() => {
+    subjectModalOverlay.classList.add("hidden");
+    subjectModalOverlay.classList.remove("flex");
+  }, 250); // fade-out animation time
+}
+
+// Buttons
+closeModal.addEventListener("click", hideModal);
+cancelModal.addEventListener("click", hideModal);
+
+// Click outside to close
+subjectModalOverlay.addEventListener("click", (e) => {
+  if (e.target === subjectModalOverlay) hideModal();
+});
+
+
+
